4362dfb240d72929760600da2bf9bf6b97817211
Raymond Gregory <vinedsel@gmail.com>
2018-04-13 15:55:26 -0500

    Updated these files.

--- index.js
+++ index.js
@@ -1,40 +1,35 @@
 const express = require('express');
-const Sequelize = require("Sequelize");
 const sqlite3 = require('sqlite3').verbose();
 const handlebars = require("express-handlebars").create({ defaultLayout: 'main' });
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'sqlite',
-  storage: "./Chinook_Sqlite_AutoIncrementPKs.sqlite"
-});
+
+
 
 const app = express();
 
 const db = new sqlite3.Database("./Chinook_Sqlite_AutoIncrementPKs.sqlite");
 
+
+
+
 app.use(require("body-parser")());
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-const Artist = sequelize.define(
-  "Artist",
-  {
-    ArtistId: {
-      type: Sequelize.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    Name: Sequelize.STRING
-  },
-  {
-    freezeTableName: true,
-    timestamps: false
-  }
-);
 
-Artist.find({ where: { ArtistId: 75 } }).then(artists => {
-  console.log(JSON.stringify(artists));
+
+
+
+
+app.get('/viewer', (req, res) => {
+    const query = `SELECT Artist.Name as Artist, Album.Title as Album FROM Artist JOIN Album WHERE Artist.ArtistId=Album.ArtistId LIMIT 1000`;
+    let resultsArray = [];
+    db.each(query, (err, row) => {
+        if (err) throw err;
+        // console.log(row);
+        resultsArray.push(row);
+      });
+    res.render('viewer', {results: resultsArray});
 });
 
 
