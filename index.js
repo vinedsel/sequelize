792212379ed34210a380e6efa205b718edabf4cb
Raymond Gregory <vinedsel@gmail.com>
2018-04-14 05:32:00 -0500

    Stated variables.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const Sequelize = require("Sequelize");
 const sqlite3 = require('sqlite3').verbose();
-const handlebars = require("express-handlebars").create({ defaultLayout: 'main' });
+const handlebars = require("express-handlebars").create({
+  defaultLayout: 'main'
+});
 
 const sequelize = new Sequelize('database', 'username', 'password', {
   host: 'localhost',
@@ -19,37 +21,46 @@ app.set('view engine', 'handlebars');
 
 
 
-app.get('/view', (req, res) => {
-    const query = `SELECT Artist.Name as Artist, Album.Title as Album FROM Artist JOIN Album WHERE Artist.ArtistId=Album.ArtistId LIMIT 1000`;
-    let resultsArray = [];
-    db.each(query, (err, row) => {
-        if (err) throw err;
-        // console.log(row);
-        resultsArray.push(row);
-      });
-    res.render('view', {results: resultsArray});
-});
-
+// app.get('/view', (req, res) => {
+//     const query = `SELECT Artist.Name as Artist, Album.Title as Album FROM Artist JOIN Album WHERE Artist.ArtistId=Album.ArtistId LIMIT 1000`;
+//     let resultsArray = [];
+//     db.each(query, (err, row) => {
+//         if (err) throw err;
+//         // console.log(row);
+//         resultsArray.push(row);
+//       });
+//     res.render('view', {results: resultsArray});
+// });
 
 
 const Artist = sequelize.define(
-  "Artist",
-  {
+  "Artist", {
     ArtistId: {
       type: Sequelize.INTEGER,
       autoIncrement: true,
       primaryKey: true
     },
     Name: Sequelize.STRING
-  },
-  {
+  }, {
     freezeTableName: true,
     timestamps: false
-  }
-);
+  });
+
+const Album = sequelize.define(
+  "Album", {
+    ArtistId: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: false
+    },
+    Title: Sequelize.STRING
+  }, {
+    freezeTableName: true,
+    timestamps: false
+  });
 
 
 
-app.listen( process.env.PORT || 3000, ()=>{
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
